a7a70e18a2dfacc6d2e0851b37a8bb6dad544261
Sharjeel Khalid <sharjeelac@gmail.com>
2025-12-20 22:22:20 +0500

    Day 33  ; Tailwind Intro

---
+++ Day_33/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      Hello World
+    </div>
+  );
+};
+
+export default App;
